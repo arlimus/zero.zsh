27b8dba0ba26533951b98b51fbd34a4e46ad4370
Dominik Richter <dominik.richter@gmail.com>
2013-01-24 14:02:26 +0100

    updated installation script to work from sh and correctly identify the users folder
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env zsh
-cp -r -t ~/.oh-my-zsh/ plugins themes
-[ -z "$(grep plugins ~/.zshrc | grep zero)" ] && echo "add zero to your plugins in ~/.zshrc"
-[ -z "$(grep ZSH_THEME ~/.zshrc | grep zero)" ] && echo "set your ZSH_THEME to zero in ~/.zshrc"
+#!/bin/sh
+
+DST="$(readlink -f ~)"
+OHMY="$DST/.oh-my-zsh"
+[ ! -d "$OHMY" ] && echo "Could not find oh-my-zsh in '$OHMY'. Make sure you install oh-my-zsh before trying to install this theme and plugin." && exit 0
+
+cp -rv -t "$OHMY" plugins themes
+echo "installed zero zsh theme and plugin to '$DST'"
+
+[ -z "$(grep plugins ~/.zshrc | grep zero)" ] && echo "please add zero to your plugins in ~/.zshrc to use it"
+[ -z "$(grep ZSH_THEME ~/.zshrc | grep zero)" ] && echo "please set your ZSH_THEME to zero in ~/.zshrc to use it"
