4e3c66c9f3c7b908b7eef15bdbbbb5bda08ae226
Dominik Richter <dominik.richter@gmail.com>
2013-02-07 16:36:58 +0100

    find .oh-my-zsh via .zshrc + improve output given to the user during installation
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-DST="$(readlink -f ~)"
-OHMY="$DST/.oh-my-zsh"
-[ ! -d "$OHMY" ] && echo "Could not find oh-my-zsh in '$OHMY'. Make sure you install oh-my-zsh before trying to install this theme and plugin." && exit 0
+# read .zshrc to find path to oh-my-zsh
+ZSHRC="$HOME/.zshrc"
 
-cp -rv -t "$OHMY" plugins themes
-echo "installed zero zsh theme and plugin to '$DST'"
+[ ! -f "$ZSHRC" ] && echo "can't find '$ZSHRC', aborting" && exit 0
 
-[ -z "$(grep plugins ~/.zshrc | grep zero)" ] && echo "please add zero to your plugins in ~/.zshrc to use it"
-[ -z "$(grep ZSH_THEME ~/.zshrc | grep zero)" ] && echo "please set your ZSH_THEME to zero in ~/.zshrc to use it"
+echo "-- loading your configuration file: '$ZSHRC'"
+source "$ZSHRC" > /dev/null 2>&1
+OHMY="$ZSH"
+[ ! -d "$OHMY" ] && echo "ee Could not find oh-my-zsh in '$OHMY'. Make sure you install 'oh-my-zsh' before trying to install this theme and plugin." && exit 0
+
+echo "-- copying files to oh-my-zsh in: '$OHMY'"
+cp -r -t "$OHMY" plugins themes
+echo "ok installed zero zsh successfully (theme: zero, plugins: zero)"
+
+[ -z "$(grep plugins ~/.zshrc | grep zero)" ] && echo "   please add zero to your plugins in ~/.zshrc to use it"
+[ -z "$(grep ZSH_THEME ~/.zshrc | grep zero)" ] && echo "   please set your ZSH_THEME to zero in ~/.zshrc to use it"
